1dfb42f39fb645864de81fdb8488560258cfb16a
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2016-10-11 23:49:09 -0400

    set correct content-typr for json files on aws s3 sync

--- src/pages/Seasons.vue
+++ src/pages/Seasons.vue
@@ -10,6 +10,7 @@
             <span>{{ tournament.location }}</span>
             <span class="right"><span>{{ tournament.start }}</span> - <span>{{ tournament.end }}</span></span><br/>
             <span class="results" v-if="tournament.OPEN"><a :href="tournament.OPEN" target="_blank">Open</a></span>
+            <span class="results" v-if="tournament.U2200"><a :href="tournament.U2200" target="_blank">U2200</a></span>
             <span class="results" v-if="tournament.U1900"><a :href="tournament.U1900" target="_blank">U1900</a></span>
             <span class="results" v-if="tournament.U1600"><a :href="tournament.U1600" target="_blank">U1600</a></span>
             <span class="results" v-if="tournament.U16"><a :href="tournament.U16" target="_blank">U16</a></span>
