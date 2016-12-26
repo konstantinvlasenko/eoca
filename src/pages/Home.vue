49d97c2f5464931af288a70860a1ac49e0fc2f0d
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2016-12-25 22:24:47 -0500

    show less

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,25 +1,13 @@
 <template>
-  <div class="row">
-    <div class="col s12 m6">
-      <schedule></schedule>
-    </div>
-    <div class="col s12 m6">
-      <standings></standings>
-      <winners></winners>
-    </div>
-  </div>
+  <schedule></schedule>
 </template>
 
 <script>
 import Schedule from '../components/Schedule'
-import Standings from '../components/Standings'
-import Winners from '../components/Winners'
 
 export default {
   components: {
-    Schedule,
-    Standings,
-    Winners
+    Schedule
   }
 }
 </script>
