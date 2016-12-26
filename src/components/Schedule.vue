ce4fe38f859e71a78a1c5712ea5c59e6154f6463
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2016-12-25 22:24:34 -0500

    remove hardcoded data

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -5,9 +5,45 @@
       <transition-group name="flip-list" tag="ul" class="collection">
         <li class="collection-item avatar" v-for="tournament in tournaments" v-bind:key="tournament.start">
           <i class="material-icons circle" v-bind:class="{ green: tournament.next }">{{ Date.now() > Date.parse(tournament.end) ? 'done' : tournament.next ? 'alarm_on' : 'schedule' }}</i>
-          <span>{{ tournament.name }}</span><br/>
-          <span>{{ tournament.location }}</span>
-          <span class="right"><span>{{ tournament.start }}</span> - <span>{{ tournament.end }}</span></span><br/>
+          <span>{{ tournament.name }}</span><span class="right"><span>{{ tournament.start }}</span> - <span>{{ tournament.end }}</span></span><br/>
+          <span>{{ tournament.location }}</span><br/>
+          <div class="blue-grey-text text-darken-2">
+            <blockquote v-if="tournament.next">
+              <div v-if="tournament.place">
+                <span><i>{{ tournament.place }}</i></span>
+              </div>
+              <div v-if="tournament.type">
+                <b>{{ tournament.type }}</b> (CFC or FQE membership is required)
+                <li class="collection-item" v-if="tournament.time">{{ tournament.time }}</li>
+              </div>
+              <div v-if="tournament.sections">
+                <b>Sections:</b>
+                <li class="collection-item" v-for="(val, key) in tournament.sections">{{ key }}: {{ val }}</li>
+              </div>
+              <div v-if="tournament.rounds">
+                <b>Rounds:</b>
+                <li class="collection-item">{{ tournament.rounds }}</li>
+              </div>
+              <div v-if="tournament.registration">
+                <b>Registration:</b>
+                <li class="collection-item" v-for="reg in tournament.registration">{{ reg }}</li>
+              </div>
+              <div v-if="tournament.fees">
+                <b>Entry Fee:</b>
+                <li class="collection-item" v-for="(val, key) in tournament.fees">{{ key }}: {{ val }}</li>
+              </div>
+              <div v-if="tournament.notes">
+                <b>Notes:</b>
+                <li class="collection-item" v-for="note in tournament.notes">{{ note }}</li>
+              </div>
+              <div v-if="tournament.byes">
+                <b>Requesting byes:</b> {{ tournament.byes }}
+              </div>
+              <div v-if="tournament.td">
+                <b>TD:</b> {{ tournament.td }}
+              </div>
+            </blockquote>
+          </div>
           <span class="results" v-if="tournament.OPEN"><a :href="tournament.OPEN" target="_blank">Open</a></span>
           <span class="results" v-if="tournament.U1900"><a :href="tournament.U1900" target="_blank">U1900</a></span>
           <span class="results" v-if="tournament.U1600"><a :href="tournament.U1600" target="_blank">U1600</a></span>
@@ -21,30 +57,19 @@
 export default {
   data () {
     return {
-      tournaments: [
-      { name: 'Almonte Open', location: 'Almonte', ref: 'resources/2016/1st-Almonte-Open.pdf', start: '2016-08-27', end: '2016-08-28', OPEN: 'http://chess.ca/crosstable?tournament_check_number=201608065' },
-      { name: 'RA Fall Open', location: 'Ottawa', ref: 'resources/2016/2016-RA-Fall-Open.pdf', start: '2016-09-23', end: '2016-09-25', OPEN: 'http://chess.ca/crosstable?tournament_check_number=201609031', U1900: 'http://chess.ca/crosstable?tournament_check_number=201609033', U1600: 'http://chess.ca/crosstable?tournament_check_number=201609032' },
-      { name: 'National Capital Open', location: 'Ottawa', ref: 'resources/2016/2016-National-Capital-Open.pdf', start: '2016-10-28', end: '2016-10-30' },
-      { name: 'RA November Open', location: 'Ottawa', start: '2016-11-25', end: '2016-11-27' },
-      { name: 'RA Winter Open', location: 'Ottawa', start: '2017-01-13', end: '2017-01-15' },
-      { name: 'Gatineau Open', location: 'Gatineau', start: '2017-03-03', end: '2017-03-05' },
-      { name: 'Kingston Open', location: 'Kingston', start: '2017-03-31', end: '2017-04-02' },
-      { name: 'CYCC Qualifier', location: 'Nepean', start: '2017-04-17', end: '2017-04-17' },
-      { name: 'Arnprior Open', location: 'Arnprior', ref: 'resources/2017/Arnprior-Open.pdf', start: '2017-04-29', end: '2017-04-30' },
-      { name: 'Ontario Open', location: 'Ottawa', start: '2017-05-20', end: '2017-05-22' },
-      { name: 'Eastern Ontario Open', location: 'Ottawa', start: '2017-06-23', end: '2017-06-25' }]
+      tournaments: {}
     }
   },
-  methods: {
-    moveCompletedToBottom: function () {
+  created () {
+    this.$http.get('//s3.amazonaws.com/eoca/static/seasons.json').then((response) => {
+      this.tournaments = response.body['2016/2017']
       let _pastTournaments = this.tournaments.filter((tournament) => { return Date.now() > Date.parse(tournament.end) })
       let _upcommingTournaments = this.tournaments.filter((tournament) => { return Date.now() < Date.parse(tournament.end) })
       this.tournaments = _upcommingTournaments.concat(_pastTournaments)
       this.tournaments[0].next = true
-    }
-  },
-  created: function () {
-    setTimeout(this.moveCompletedToBottom, 3000)
+    }, (response) => {
+      console.log(response)
+    })
   }
 }
 </script>
