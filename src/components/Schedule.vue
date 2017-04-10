9de1743dded157b418f591eff76231c500d8b4b9
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2017-04-09 22:01:57 -0400

    simplify

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -2,82 +2,70 @@
   <div class="card">
     <div class="card-content">
       <span class="card-title">Grand Prix Tournaments 2016/2017</span><i class="right material-icons">event_available</i>
-      <transition-group name="flip-list" tag="ul" class="collection">
-        <li class="collection-item avatar" v-for="tournament in tournaments" v-bind:key="tournament.start">
-          <i class="material-icons circle" v-bind:class="{ green: tournament.next }">{{ Date.now() > Date.parse(tournament.end) ? 'done' : tournament.next ? 'alarm_on' : 'schedule' }}</i>
-          <span>{{ tournament.name }}</span><span class="right"><span>{{ tournament.start }}</span> - <span>{{ tournament.end }}</span></span><br/>
-          <span>{{ tournament.location }}</span><br/>
-          <span v-if="tournament.document"><a :href="tournament.document">Information</a></span><br/>
-          <div class="blue-grey-text text-darken-2">
-            <blockquote v-if="tournament.next">
-              <div v-if="tournament.place">
-                <span><i>{{ tournament.place }}</i></span>
-              </div>
-              <div v-if="tournament.type">
-                <b>{{ tournament.type }}</b> (CFC or FQE membership is required)
-                <li class="collection-item" v-if="tournament.time">{{ tournament.time }}</li>
-              </div>
-              <div v-if="tournament.sections">
-                <b>Sections:</b>
-                <li class="collection-item" v-for="(val, key) in tournament.sections">{{ key }}: {{ val }}</li>
-              </div>
-              <div v-if="tournament.rounds">
-                <b>Rounds:</b>
-                <li class="collection-item">{{ tournament.rounds }}</li>
-              </div>
-              <div v-if="tournament.registration">
-                <b>Registration:</b>
-                <li class="collection-item" v-for="reg in tournament.registration">{{ reg }}</li>
-              </div>
-              <div v-if="tournament.fees">
-                <b>Entry Fee:</b>
-                <li class="collection-item" v-for="(val, key) in tournament.fees">{{ key }}: {{ val }}</li>
-              </div>
-              <div v-if="tournament.notes">
-                <b>Notes:</b>
-                <li class="collection-item" v-for="note in tournament.notes">{{ note }}</li>
-              </div>
-              <div v-if="tournament.byes">
-                <b>Requesting byes:</b> {{ tournament.byes }}
-              </div>
-              <div>
-                <b>Electronic Devices:</b>
-                <li class="collection-item">A mobile phone or other electronic device is allowed to be stored in a player’s bag or purse, as long as the device is completely switched off. A player is forbidden to carry a bag holding such a device, without the permission of the arbiter.</li>
-              </div>
-              <div v-if="tournament.td">
-                <b>Tournament Director:</b> {{ tournament.td }}
-              </div>
-              <div v-if="tournament.to">
-                <b>Tournament Organizer:</b> {{ tournament.to }}
-              </div>
-            </blockquote>
+      <ul class="collapsible popout" data-collapsible="accordion">
+        <li>
+          <div class="collapsible-header active"><i class="material-icons right"></i>Upcoming</div>
+          <div class="collapsible-body">
+            <ul class="collection">
+              <li class="collection-item avatar" v-for="tournament in tournaments" v-bind:key="tournament.start">
+                <i class="material-icons circle" v-bind:class="{ green: tournament.next }">{{ Date.now() > Date.parse(tournament.end) ? 'done' : tournament.next ? 'alarm_on' : 'schedule' }}</i>
+                <span>{{ tournament.name }}</span>
+                <span class="right"><span v-if="tournament.document"><a :href="tournament.document">Information</a></span></span><br/>
+                <span>{{ tournament.location }}</span>
+                <span class="right"><span>{{ tournament.start }}</span> - <span>{{ tournament.end }}</span></span><br/>
+              </li>
+            </ul>
           </div>
-          <span class="results" v-if="tournament.OPEN"><a :href="tournament.OPEN" target="_blank">Open</a></span>
-          <span class="results" v-if="tournament.U1900"><a :href="tournament.U1900" target="_blank">U1900</a></span>
-          <span class="results" v-if="tournament.U1600"><a :href="tournament.U1600" target="_blank">U1600</a></span>
         </li>
-      </transition-group>
+        <li>
+          <div class="collapsible-header"><i class="material-icons right"></i>Completed</div>
+          <div class="collapsible-body">
+            <ul class="collection">
+              <li class="collection-item avatar" v-for="tournament in completed" v-bind:key="tournament.start" v-if="Date.now() > Date.parse(tournament.end)">
+                <i class="material-icons circle">done</i>
+                <span>{{ tournament.name }}</span><br/>
+                <span>{{ tournament.location }}</span>
+                <span class="right"><span>{{ tournament.start }}</span> - <span>{{ tournament.end }}</span></span><br/>
+                <span class="results" v-if="tournament.OPEN"><a :href="tournament.OPEN" target="_blank">Open</a></span>
+                <span class="results" v-if="tournament.U2200"><a :href="tournament.U2200" target="_blank">U2200</a></span>
+                <span class="results" v-if="tournament.U1900"><a :href="tournament.U1900" target="_blank">U1900</a></span>
+                <span class="results" v-if="tournament.U1800"><a :href="tournament.U1800" target="_blank">U1800</a></span>
+                <span class="results" v-if="tournament.U1600"><a :href="tournament.U1600" target="_blank">U1600</a></span>
+                <span class="results" v-if="tournament.U1500"><a :href="tournament.U1500" target="_blank">U1500</a></span>
+                <span class="results" v-if="tournament.U16"><a :href="tournament.U16" target="_blank">U16</a></span>
+                <span class="results" v-if="tournament.U12"><a :href="tournament.U12" target="_blank">U12</a></span>
+                <span class="results" v-if="tournament.U10"><a :href="tournament.U10" target="_blank">U10</a></span>
+                <span class="results" v-if="tournament.U8"><a :href="tournament.U8" target="_blank">U8</a></span>
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+/* global $ */
 export default {
   data () {
     return {
-      tournaments: {}
+      tournaments: {},
+      completed: {}
     }
   },
   created () {
     this.$http.get('/static/seasons.json').then((response) => {
       this.tournaments = response.body['2016/2017']
-      let _pastTournaments = this.tournaments.filter((tournament) => { return Date.now() > Date.parse(tournament.end) })
-      let _upcommingTournaments = this.tournaments.filter((tournament) => { return Date.now() < Date.parse(tournament.end) })
-      this.tournaments = _upcommingTournaments.concat(_pastTournaments)
+      this.completed = this.tournaments.filter((tournament) => { return Date.now() > Date.parse(tournament.end) })
+      this.tournaments = this.tournaments.filter((tournament) => { return Date.now() < Date.parse(tournament.end) })
       this.tournaments[0].next = true
     }, (response) => {
       console.log(response)
     })
+  },
+  mounted () {
+    $(this.$el).collapsible()
   }
 }
 </script>
@@ -91,6 +79,14 @@ export default {
   }
   .flip-list-move {
     transition: transform 1.5s;
+  }
+
+  .collapsible-header >i::after {
+    content: 'expand_more'
+  }
+  
+  .collapsible-header.active >i::after {
+    content: 'expand_less'
   }
 </style>
 
