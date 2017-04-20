054f8ff5d24aaaa542d7ffcba42565e1ecb0f904
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2017-04-20 19:06:20 -0400

    announcement

---
+++ src/components/Announcement.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="card light-blue lighten-2">
+    <div class="card-content white-text">
+      <span class="card-title">2017 EOCA AGM, 7th May 2017, 11am, RA Centre</span><i class="right material-icons">announcement</i>
+      <p>Great brunch buffet on sale, fun Active tournament afterwards!</p>
+    </div>
+  </div>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,18 @@
 <template>
-  <schedule></schedule>
+  <div>
+    <announcement></announcement>
+    <schedule></schedule>
+  </div>
 </template>
 
 <script>
 import Schedule from '../components/Schedule'
+import Announcement from '../components/Announcement'
 
 export default {
   components: {
-    Schedule
+    Schedule,
+    Announcement
   }
 }
 </script>
