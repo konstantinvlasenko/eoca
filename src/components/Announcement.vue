f833831ea8512dd6c50413338caab2309d0d4049
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2018-05-08 20:58:47 -0400

    anouncement

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="card light-blue lighten-2">
     <div class="card-content white-text">
-      <span class="card-title">2017 EOCA AGM, 7th May 2017, 11am, RA Centre</span><i class="right material-icons">announcement</i>
-      <p>Great brunch buffet on sale, fun Active tournament afterwards!</p>
+      <span class="card-title">2017 EOCA AGM, 31st May 2018, 630pm, RA Centre, chess room</span><i class="right material-icons">announcement</i>
+      <p>Striving to be done in ONE hour, fun rated pickup game afterwards!</p>
     </div>
   </div>
 </template>
