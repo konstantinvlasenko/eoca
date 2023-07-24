63f835c8d374ce9b1e27ea388f3a98bc4641a826
Konstantin Vlasenko <konstantin@vlasenko.org>
2023-07-23 20:55:12 -0400

    sizing

--- src/components/TournamentsSchedule.vue
+++ src/components/TournamentsSchedule.vue
@@ -1,6 +1,6 @@
 <template>
   <p class="text-center text-h6">Grand Prix Tournaments 2023/2024</p>
-  <v-container class="w-75">
+  <v-container>
     <v-row :key="start" v-for="{ name, location, start, end, document, next } in tournaments" class="bg-indigo-lighten-5 ma-auto">
       <v-col cols="1" class="float-right"><v-icon icon="mdi-clock-outline" color="green"></v-icon></v-col>
       <v-col cols="7"><span>{{ name }}</span><br/><span>{{ location }}</span><br/><span v-if="document"><a :href="document">Information</a></span></v-col>
@@ -15,8 +15,8 @@
   const tournaments = ref([]);
 
   async function getData() {
-    //const res = await fetch("/static/seasons.json");
-    const res = await fetch("https://eoca.s3.amazonaws.com/site/static/seasons.json");
+    const res = await fetch("/static/seasons.json");
+    //const res = await fetch("https://eoca.s3.amazonaws.com/site/static/seasons.json");
     tournaments.value = (await res.json())['2023/2024'];
   }
 
