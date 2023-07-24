5b2af0e9050fe4ece733149198fe573a901ddc62
Konstantin Vlasenko <konstantin@vlasenko.org>
2023-07-23 20:11:20 -0400

    seasons

--- src/components/TournamentsSchedule.vue
+++ src/components/TournamentsSchedule.vue
@@ -1,13 +1,13 @@
 <template>
   <p class="text-center text-h6">Grand Prix Tournaments 2023/2024</p>
   <v-container>
-    <v-row :key="start" v-for="{ name, location, start, end, next } in tournaments" class="bg-indigo-lighten-5 ma-auto">
+    <v-row :key="start" v-for="{ name, location, start, end, document, next } in tournaments" class="bg-indigo-lighten-5 ma-auto">
       <v-col cols="1"><v-icon icon="mdi-clock-outline" color="green"></v-icon></v-col>
       <v-col cols="4">{{ name }}</v-col>
-      <v-col cols="3">{{ location }}</v-col>
+      <v-col cols="2">{{ location }}</v-col>
       <v-col cols="2">{{ start }}</v-col>
-      <v-col cols="2">
-        <div :class="rounded-r-0">{{ end }}</div></v-col>
+      <v-col cols="2">{{ end }}</v-col>
+      <v-col cols="1"><span v-if="document"><a :href="document">Information</a></span></v-col>
     </v-row>
   </v-container>
 </template>
@@ -19,6 +19,7 @@
 
   async function getData() {
     const res = await fetch("/static/seasons.json");
+    //const res = await fetch("https://eoca.s3.amazonaws.com/site/static/seasons.json");
     tournaments.value = (await res.json())['2023/2024'];
   }
 
