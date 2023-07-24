a17b3686812c73d3673f8fb9f45c07148d655c79
Konstantin Vlasenko <konstantin@vlasenko.org>
2023-07-23 20:54:58 -0400

    sizing

--- src/components/TournamentsSchedule.vue
+++ src/components/TournamentsSchedule.vue
@@ -3,9 +3,8 @@
   <v-container class="w-75">
     <v-row :key="start" v-for="{ name, location, start, end, document, next } in tournaments" class="bg-indigo-lighten-5 ma-auto">
       <v-col cols="1" class="float-right"><v-icon icon="mdi-clock-outline" color="green"></v-icon></v-col>
-      <v-col cols="5"><span>{{ name }}</span><br/><span>{{ location }}</span><br/><span v-if="document"><a :href="document">Information</a></span></v-col>
-      <v-col cols="3">{{ start }}</v-col>
-      <v-col cols="3">{{ end }}</v-col>
+      <v-col cols="7"><span>{{ name }}</span><br/><span>{{ location }}</span><br/><span v-if="document"><a :href="document">Information</a></span></v-col>
+      <v-col cols="4"><span>{{ start }}</span><br/><span>{{ end }}</span></v-col>
     </v-row>
   </v-container>
 </template>
@@ -16,8 +15,8 @@
   const tournaments = ref([]);
 
   async function getData() {
-    const res = await fetch("/static/seasons.json");
-    //const res = await fetch("https://eoca.s3.amazonaws.com/site/static/seasons.json");
+    //const res = await fetch("/static/seasons.json");
+    const res = await fetch("https://eoca.s3.amazonaws.com/site/static/seasons.json");
     tournaments.value = (await res.json())['2023/2024'];
   }
 
