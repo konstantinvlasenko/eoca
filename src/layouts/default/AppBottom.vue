6d3c5624e5747ed3b1272087bfce58c5b5da4806
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2023-11-30 19:39:18 -0500

    update

--- src/layouts/default/AppBottom.vue
+++ src/layouts/default/AppBottom.vue
@@ -1,30 +1,17 @@
 <template>
   <v-footer class="bg-indigo-lighten-1">
     <v-row justify="center" no-gutters>
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        variant="text"
-        class="mx-2"
-        rounded="xl"
-      >
-        {{ link }}
-      </v-btn>
+      <router-link  to="/" v-slot="{ navigate }">
+        <v-btn color="white" variant="text" class="mx-2" rounded="xl" @click="navigate">Home</v-btn>
+      </router-link>
+      <router-link  to="/constitution" v-slot="{ navigate }">
+        <v-btn color="white" variant="text" class="mx-2" rounded="xl" @click="navigate">Constitution</v-btn>
+      </router-link>
       <v-col class="text-center mt-4" cols="12">
         {{ new Date().getFullYear() }} — <strong>eoca</strong>
       </v-col>
     </v-row>
   </v-footer>
 </template>
-
-<script>
-  export default {
-    data: () => ({
-      links: [
-        'Home',
-        'Constitution',
-      ],
-    }),
-  }
+<script setup>
 </script>
